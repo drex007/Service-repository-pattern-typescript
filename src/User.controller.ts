ce66b70c49225b49aec22d7f3d781b0d19822c83
drex007 <vykeoj@gmail.com>
2023-10-17 22:57:12 +0000

    service repo pattern

--- src/User.controller.ts
+++ src/User.controller.ts
@@ -2,4 +2,14 @@ import { UserRequestDto } from './Database'
 import { UserService } from './User.service'
 
 // NOTE: Make userService public and not private. This is required for the tests!
-export class UserController {}
+export class UserController {
+  constructor(public readonly userService: UserService){
+
+  }
+
+  async store(requestData:UserRequestDto){
+    return this.userService.createUser(requestData)
+
+  }
+}
+ 
